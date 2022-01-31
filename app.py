3e0a37f9b75f7fd621247242ad20017f20c5fb2f
OscarGreen03 <greenorh@icloud.com>
2022-01-31 11:00:48 +0000

    Hash Mounted Counter-Honeypot Matrix Remapping to deter md5 hash brute force algorithmic calculation via hypervisor virtualisation.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import flash
+
 from random import randint
 import hashlib
 
@@ -17,9 +19,12 @@ def initial():
         inputfield = request.form['title']
         inputfield = hashlib.md5(inputfield.encode('utf-8')).hexdigest()
 
-
+        print(inputfield)
         if inputfield == "311354ef5c1a158fbf779f48bed47eed":
             name = bigalt[randint(0,len(bigalt))-1] + cashalt[randint(0,len(cashalt))-1]
+        if inputfield == "d41d8cd98f00b204e9800998ecf8427e":
+            name = ""
         else:
-            name = ''
+            name = "INTRUDER DETECTED RUNNING RAM DUMP"
+
     return render_template('namegen.html', name = name)
